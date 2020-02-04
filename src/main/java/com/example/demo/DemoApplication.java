a36e05eadcd5309589267a6ab1f1d194403580fe
tavisca-apaithane <apaithane@tavisca.in>
2020-02-04 15:47:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Autowired
+	UserRepository userRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(){
+		return args -> {
+			User user = new User();
+			user.setFirstName("graphql");
+			user.setLastName("annotation-configuration");
+			userRepository.save(user);
+		};
+	}
+}
---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@GraphQLApi
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    @GraphQLQuery
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    @GraphQLMutation
+    public void addUser(String firstName, String lastName){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        userRepository.save(user);
+    }
+}
