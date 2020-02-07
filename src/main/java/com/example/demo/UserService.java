d2d7355fbc19c9aa534e87272e2d5d4a741caefe
tavisca-apaithane <apaithane@tavisca.in>
2020-02-07 14:16:28 +0530

    a commit

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -6,6 +6,7 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,11 +18,15 @@ public class UserService {
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
-    @GraphQLMutation
-    public void addUser(String firstName, String lastName){
+    @GraphQLMutation()
+    public User addUser(String firstName, String lastName){
         User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         userRepository.save(user);
+//        List<User> list = new ArrayList();
+//        list.add(user);
+        return user;
     }
+
 }
